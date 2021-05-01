f0edddeb52dcf807fc974d3227774dd279d52065
Nawar <Nawar-nar@hotmail.com>
2021-05-01 10:30:52 +0300

    Fix closing animation for accordion items

--- js/index.js
+++ js/index.js
@@ -1,15 +1,10 @@
 $(document).ready(function () {
-  $("details").click(function () {
-    $("details").not(this).removeAttr("open");
+  // $("details").click(function () {
+  //   $("details").not(this).removeAttr("open");
+  // });
+
+  $(".accordion-item").click(function () {
+    $(".accordion-item").not(this).find(".accordion-answer").removeClass("accordion-answer-open");
+    $(".accordion-answer", this).toggleClass("accordion-answer-open accordion-answer-close");
   });
-
-  $("details").hover(
-    function () {
-      $(".illustraion-box").css({"transition": "0.25s transform ease", "transform": "translate(-30px)"});
-    },
-    function () {
-      $(".illustraion-box").css({"transform": "translate(0)"});
-    }
-  );
 });
-
